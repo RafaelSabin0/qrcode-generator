f2a376a37577439dbd8210ef62620ad1a865381b
RafaelSabin0 <rafaelarrudaolimpio@gmail.com>
2023-02-19 15:34:11 -0300

    added download option

--- src/components/qrcode-image/index.jsx
+++ src/components/qrcode-image/index.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import QRCode from 'react-qr-code';
+import { toPng } from 'html-to-image';
 import './styles.css';
 
 
 export default function QRcodeResult (props){
-    console.log(props.inputValue.length);
 
-    if(props.inputValue.length > 0){
+    const canvas = document.getElementById("qr-code-img");
     
-       return <QRCode className="qrcode-result" value={props.inputValue}/>
+    const downloadQr = () =>{
+      if(canvas === null) {
+         return
+      }
+      console.log(canvas)
+      toPng(canvas)
+      .then((dataUrl) => {
+         const link = document.createElement('a');
+         link.style.display = 'none';
+         link.href = dataUrl;
+         link.download = 'mything.png';
+         link.click();
+      })
+
+   } 
+
+    if(props.inputValue.length === 0){
+      return <div className="message-box"></div>
     }
+
+    return (
+      <>
+      <QRCode id="qr-code-img" className="qrcode-result" value={props.inputValue}/>
+      <div className="button-box">
+      <Button className="download-button" onClick={downloadQr}>Download QRcode</Button>
+      </div>
+      </>
+
+      )
    
-       return <div className="message-box"></div>
    
 }
 
